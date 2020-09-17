51b4a2ec9467d8cb0ac8c7fa6030a63dfb018aa8
Sigurd Øvre Bjørndal <s333741@oslomet.no>
2020-09-17 12:28:28 +0200

    ss

---
+++ KundeApp2/Model/KundeContekst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace KundeApp2.Model
+{
+    public class KundeContekst : DbContext
+    {
+            public KundeContekst (DbContextOptions<KundeContekst> options)
+                    : base(options)
+            {
+                Database.EnsureCreated();
+            }
+
+        public DbSet<Kunde> Kunder { get; set; }
+    }
+}
